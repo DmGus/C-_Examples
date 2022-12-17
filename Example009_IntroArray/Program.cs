46abe342393ab00bf66e435ce8c19f0d5af2f291
Dmitrii Gushchin <dgushchin@gmail.com>
2022-12-17 16:54:34 +0300

    Add examples from 2 lrcture

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int[] array = { 11, 21, 31, 4, 11, 61, 17, 11, 19 };
+
+int n = array.Length;
+int find = 11;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
